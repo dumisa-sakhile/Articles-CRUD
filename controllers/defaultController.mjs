3c17712139a0a0b29bde2791ea21103d27ec89e7
dumisa-sakhile <dumisa.developer@gmail.com>
2024-04-21 17:09:41 +0200

    Add default router pages

--- controllers/defaultController.mjs
+++ controllers/defaultController.mjs
@@ -4,3 +4,10 @@ export const getHome = async (req, res) => {
   };
   res.status(200).render("index", data);
 };
+
+export const getNew = async (req, res) => {
+  const data = {
+    title: "Add a new article",
+  };
+  res.status(200).render("new", { data ,layout : "./layouts/dashboard.ejs"});
+};
--- routes/defaultRouter.mjs
+++ routes/defaultRouter.mjs
@@ -1,11 +1,10 @@
 import express from "express";
 
-import {
-  getHome
-} from "../controllers/defaultController.mjs";
+import { getHome, getNew } from "../controllers/defaultController.mjs";
 
 const router = express.Router();
 
 router.get("/", getHome);
+router.get("/articles/new", getNew);
 
 export default router;
